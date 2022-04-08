9069d35eb5c0d51156b554c669f83359abee070d
cemil <cemiw033@gmail.com>
2022-04-08 10:41:44 +0400

    proxy

---
+++ Proxy/Interface1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proxy
+{
+    public interface ISubject
+    {
+        void Request();
+    }
+    public class RealSubject : ISubject
+    {
+        public void Request()
+        {
+            Console.WriteLine("I am Real Subject");
+        }
+    }
+    public class Proxy : ISubject
+    {
+        private RealSubject _realSubject;
+        public Proxy(RealSubject realSubject)
+        {
+            _realSubject = realSubject;
+        }
+        public bool CheckAccess()
+        {
+            Console.WriteLine("Proxy Checking");
+            return true;
+        }
+        public void LogAccess()
+        {
+            Console.WriteLine("Logging");
+        }
+        public void Request()
+        {
+            if (CheckAccess())
+            {
+                _realSubject.Request();
+                LogAccess();
+            }
+        }
+    }
+    public class Client
+    {
+        public void ClientCode(ISubject subject)
+        { 
+            subject.Request();
+        }
+    }
+}
---
+++ Proxy/Program.cs
@@ -0,0 +1,7 @@
+﻿using Proxy;
+Client client = new Client();
+RealSubject realSubject = new RealSubject();
+client.ClientCode(realSubject);
+Console.WriteLine();
+Proxy.Proxy proxy = new Proxy.Proxy(realSubject);
+client.ClientCode(proxy);
